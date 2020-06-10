73d63df501f19a2dd0fa4d4823f7c00d16d0df57
YYY <XXX>
2020-06-10 14:04:37 -0500

    init impl and readme

--- README.md
+++ README.md
@@ -1,2 +1,51 @@
-# helm-shell-history
-emacs helm completion for shell history
+# helm-shell-history.el
+
+## Introduction
+`helm-shell-history` is a helm frontend to shell history and currently supports bash history.
+
+It works by querying `history` via shell command and presenting completion candidates in recency order.
+The sole completion action is to insert the selected command at the end of the buffer (for optionally editing and easily running the command again in the shell emulator)
+
+## Requirements
+
+* Emacs 25.3 or higher
+* Helm 1.9.9 or higher
+
+## Example Installation
+
+```bash
+mkdir -p ~/.emacs.d/lisp/helm-shell-history
+git clone https://github.com/PalaceChan/helm-shell-history.git ~/.emacs.d/lisp/helm-shell-history
+```
+
+## Example Configuration
+
+```lisp
+  (use-package helm-shell-history
+    :load-path "~/.emacs.d/lisp/helm-shell-history"
+    :config    
+    (define-key term-mode-map (kbd "M-r") 'helm-shell-history))
+```
+
+## Customize Variables
+
+#### `helm-shell-history-file` (Default `~/.bash_history`)
+
+History file to use
+
+#### `helm-shell-history-time-format` (Default `%Y%m%d %T`)
+
+Date/timestamp prefix to use when displaying candidates.
+If you override this in a way that alters the count of displayed prefix tokens make sure to also update `helm-shell-history-prefix-tokens`
+
+#### `helm-shell-history-prefix-tokens` (Default `3`)
+
+Tokens to skip when inserting selected command (the default of 3 corresponds to the default command number, date, and time)
+
+#### `helm-shell-history-fuzzy-match` (Default `nil`)
+
+Set to `t` if you want fuzzy matching of candidates.
+
+#### `helm-shell-history-candidate-limit` (Default `99999`)
+
+Limit the number of candidates to this number of most recent shell commands.
---
+++ helm-shell-history.el
@@ -0,0 +1,83 @@
+;;; helm-shell-history.el --- Shell command history helm interface -*- lexical-binding: t -*-
+
+;; Version: 0.0.0
+;; Author: Palace Chan
+;; URL: https://github.com/PalaceChan/helm-shell-history
+;; Keywords: convenience, helm, shell, history
+;; Package-Requires ((emacs "25.3") (helm "1.9.9"))
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+;; See the GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; A helm frontend for searching shell command history
+
+;;; Code:
+
+(require 'helm)
+
+(defvar helm-shell-history-candidate-limit 99999
+  "Limit search this many most recent entries in your history.")
+
+(defvar helm-shell-history-buffer "*Helm Shell History*")
+
+(defvar helm-shell-history-file "~/.bash_history")
+
+(defvar helm-shell-history-time-format "%Y%m%d %T"
+  "Equivalent of HISTTIMEFORMAT.
+ If you override this, you need to override helm-shell-history-prefix-tokens")
+
+(defvar helm-shell-history-prefix-tokens 3
+  "Set this to the number of extra tokens prepended by your history display format.
+For example if a history line looks like:
+
+ 2653 20200609 18:00:00 echo hello
+
+set it to 3 so as to skip '2653 20200609 18:00:00'")
+
+(defvar helm-shell-history-fuzzy-match nil
+  "Whether to fuzzy match for helm completion.")
+
+(defun helm-shell-history-build-source ()
+  (let ((shell-cmd
+	 (format "HISTFILE=%s; HISTTIMEFORMAT='%s '; history -r $HISTFILE; \
+history | tail -n %s | awk '{a[i++]=$0} END {for (j=i-1; j>=0;) print a[j--] }'"
+		 helm-shell-history-file helm-shell-history-time-format helm-shell-history-candidate-limit)))
+    (message "%s" shell-cmd)
+    (seq-remove #'string-blank-p 
+		(split-string (shell-command-to-string shell-cmd) "\n"))))
+
+(defun helm-shell-history-command-action (arg)
+  "Insert command portion of ARG in current buffer"  
+  (let ((cmd (string-join
+	      (nthcdr helm-shell-history-prefix-tokens 
+		      (split-string arg)) " ")))
+    (goto-char (point-max))
+    ;(skip-chars-backward "\n[:space:]")
+    (insert (s-prepend " " cmd))))
+
+;;;###autoload
+(defun helm-shell-history ()
+  "Find command in shell history."
+  (interactive)
+  (helm :sources (helm-build-sync-source "Shell history"
+		   :candidates 'helm-shell-history-build-source
+		   :fuzzy-match helm-shell-history-fuzzy-match
+		   :action 'helm-shell-history-command-action)
+	:buffer helm-shell-history-buffer
+	:prompt "Find command: "))
+
+(provide 'helm-shell-history)
+
+;;; helm-shell-history.el ends here
